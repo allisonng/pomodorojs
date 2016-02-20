2d754873016fc303df5dbf57b3bd8418d6e6aa2a
Allison Ng <aln.ng3@gmail.com>
2016-02-20 14:03:12 -0800

    25 min counter starts on page load

--- pomodoro.js
+++ pomodoro.js
@@ -3,20 +3,11 @@
 function addMinutes(oldDate, min) {
     return new Date(oldDate.getTime() + min*60000); // 60x1000ms
 }
-// need to get diff between current_time and endTime
-// function getTimeDiff();
-
-var startTime = new Date();
-var start_min = startTime.getMinutes();
-console.log('start date is' + startTime.getTime());
-
-// endTime should be 25 minutes from now
-var endTime = addMinutes(startTime, 25);
-console.log('end date is' + endTime.getTime());
 
 function getTimeRemaining(endTime){
     // figure out time remaining between end_date and startTime
-    var timeDiff = endTime.getTime() - startTime.getTime();
+    var timeDiff = endTime.getTime() - Date.parse(new Date());
+
     // convert millseconds into actual usable chunks
     var minutes = Math.floor((timeDiff/1000/60)%60);
     var seconds = Math.floor((timeDiff/1000)%60);
@@ -29,19 +20,30 @@ function getTimeRemaining(endTime){
 }
 
 
-console.log('total '+ timeDiff + '\tminutes ' + minutes + '\tseconds ' + seconds);
-
-function setTimer(id, endTime){
+function initializeClock(id, endTime){
     var timer = document.getElementById(id);
-    //NTS: setInterval executes fn continuously, endTime indicates millisecond timelength between each execution
-    var timeInterval = setInterval(function() {
-        var time = getTimeRemaining(endTime);
-        timer.innerHTML = 'minutes: ' + time.days + ' seconds:' + time.seconds;
+    var minutesSpan = timer.querySelector("#" + id.toString() + "Minutes");
+    var secondsSpan = timer.querySelector("#" + id.toString() + "Seconds");
 
-        if(time.milliseconds <= 0){
+    function updateTimer() {
+        var timeLeft = getTimeRemaining(endTime);
+        //slice(2) makes sure there's at least a 0 in front of a single digit)
+        minutesSpan.innerHTML = ('0' + timeLeft.minutes).slice(-2);
+        secondsSpan.innerHTML = ('0' + timeLeft.seconds).slice(-2);
+
+        if(timer.milliseconds <= 0){
             clearInterval(timeInterval);
-        };
-    },1000);
+        }
+    }
+
+    updateTimer();
+    var timeInterval = setInterval(updateTimer, 1000);
+
 }
 
-initializeClock('');
+var startTime = new Date();
+
+// endTime should be 25 minutes from now
+var endTime = addMinutes(startTime, 25);
+
+initializeClock('timer', endTime);
