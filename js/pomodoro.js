0be594d1121f7d547772f1a5c3ebdd6f4eb89c61
Allison Ng <aln.ng3@gmail.com>
2016-03-02 21:47:18 -0800

    Tried a stackoverflow solution, start and pause works, need to fix up resume.

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -1,3 +1,5 @@
+var timerDiv = 'timer';
+
 
 function addMinutes(oldDate, min) {
     return new Date(oldDate.getTime() + min*60000); // 60x1000ms
@@ -19,15 +21,15 @@ function getTimeRemaining(endTime){
     };
 }
 
-var timeInterval, currTime;
+var timeInterval;
 
+// parses date object into mins/secs for display
 function initializeClock(id, endTime){
     var timer = document.getElementById(id);
     var minutesSpan = timer.querySelector("#" + id.toString() + "Minutes");
     var secondsSpan = timer.querySelector("#" + id.toString() + "Seconds");
 
     function updateTimer() {
-    	currTime = endTime;
         var timeLeft = getTimeRemaining(endTime);
         //slice(2) makes sure there's at least a 0 in front of a single digit)
         minutesSpan.innerHTML = ('0' + timeLeft.minutes).slice(-2);
@@ -41,35 +43,92 @@ function initializeClock(id, endTime){
     updateTimer();
     timeInterval = setInterval(updateTimer, 1000);
 
+    return timeInterval;
 }
+
+function Timer(callback, interval) {
+    var timerId, startTime, endTime, remaining = interval;
+    var state = 0; // 0 = idle, 1 = running, 2 = paused, 3 = resumed
+
+    this.pause = function() {
+        if (state != 1) return;
+
+        remaining -= new Date() - startTime;
+        window.clearInterval(timerId);
+        state = 2;
+        console.log('paused');
+    };
+
+    this.resume = function() {
+        if (state != 2) return;
+
+        state = 3;
+        //timerId = window.setTimeout(this.timeoutCallback, remaining)
+        timerId = initializeClock(timerDiv, remaining);
+        console.log("Resuming");
+    };
+
+    this.timeoutCallback = function() {
+        if(state != 3) return;
+
+        //callback();
+
+        startTime = new Date();
+        timerId = initializeClock(timerDiv, remaining);
+        state = 1;
+        console.log('running Timer2');
+    };
+
+    startTime = new Date();
+    endTime = addMinutes(startTime, 25);
+    timerId = initializeClock(timerDiv, endTime);
+    state = 1;
+    console.log('running Timer1');
+}
+
+// to use
+/*
+var timer = new Timer(FUNCTION, 1000);
+timer.pause();
+timer.resume()
+ */
 
 
 document.getElementById("timerStart").onclick = startTimer;
-document.getElementById("timerPause").onclick = pauseTimer;
+document.getElementById("timerPause").onclick = pauseTimer
 document.getElementById("timerResume").onclick = resumeTimer;
+
+var timer;
 
 function startTimer(){
     console.log('started timer');
     document.getElementById('timerStarted').style.display = 'block';
     document.getElementById('timerDefault').style.display = 'none';
 
-    var startTime = new Date();
-// endTime should be 25 minutes from now
-    var endTime = addMinutes(startTime, 25);
+    //var startTime = new Date();
+    //var endTime = addMinutes(startTime, 25);
 
-    initializeClock('timer', endTime);
+    timer = new Timer(initializeClock, 1000);
 	
 }
 
 function pauseTimer(){
 	// need to get current date object in order to resume?
 	// clear interval, and then 'initialize clock' with the date
-	clearInterval(timeInterval);
-	console.log("cleared");
+    if(timer){
+        timer.pause();
+    }
 }
 
 function resumeTimer(){
-	initializeClock('timer', currTime);
-	console.log("reinit with timeLeft" + currTime);
-	
+    if(timer){
+        timer.resume();
+    }
 }
+
+//
+//window.setTimeout(function () {
+//    timer.pause(); window.setTimeout( function () {
+//        timer.resume();
+//    }, 5000);
+//}, 2000);
