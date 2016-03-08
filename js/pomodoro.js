12d472c45c30ba30f75db5124b814e91c7e37033
Allison Ng <aln.ng3@gmail.com>
2016-03-07 19:07:35 -0800

    Did 3 things 1. Fixed weird split second issue where timer would jump up 1 second after resuming (via windows.timeout on the remaining fraction of a second). 2. Also fixed issue where after timer finished, it would show -1:-1. 3. refactored, gave better variable names that actually made sense.

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -1,14 +1,14 @@
-var timerDiv = 'timer';
-
+var timerDiv = 'timer',
+    timeInterval = 1000,
+    fullTimerPeriod = 0.0833;
 
 function addMinutes(oldDate, min) {
     return new Date(oldDate.getTime() + min*60000); // 60x1000ms
 }
 
-
 function getTimeRemaining(endTime){
     // figure out time remaining between end_date and right now
-    var timeDiff = endTime - Date.parse(new Date());
+    var timeDiff = endTime - new Date();
 
     // convert millseconds into actual usable chunks
     var minutes = Math.floor((timeDiff/1000/60)%60);
@@ -21,14 +21,12 @@ function getTimeRemaining(endTime){
     };
 }
 
-var timeInterval;
 
 // parses date object into mins/secs for display
 function initializeClock(id, endTime){
-    console.log("initializing");
-    var timer = document.getElementById(id);
-    var minutesSpan = timer.querySelector("#" + id.toString() + "Minutes");
-    var secondsSpan = timer.querySelector("#" + id.toString() + "Seconds");
+    var timerDiv = document.getElementById(id);
+    var minutesSpan = timerDiv.querySelector("#" + id.toString() + "Minutes");
+    var secondsSpan = timerDiv.querySelector("#" + id.toString() + "Seconds");
 
     function updateTimer() {
         var timeLeft = getTimeRemaining(endTime);
@@ -36,18 +34,19 @@ function initializeClock(id, endTime){
         minutesSpan.innerHTML = ('0' + timeLeft.minutes).slice(-2);
         secondsSpan.innerHTML = ('0' + timeLeft.seconds).slice(-2);
 
-        if(timer.milliseconds <= 0){
-            clearInterval(timeInterval);
+        if(timeLeft.milliseconds <= 0){
+            clearInterval(timerId);
+            minutesSpan.innerHTML = '00';
+            secondsSpan.innerHTML = '00';
         }
     }
 
     updateTimer();
-    timeInterval = setInterval(updateTimer, 1000);
-
-    return timeInterval;
+    var timerId = setInterval(updateTimer, timeInterval);
+    return timerId;
 }
 
-function Timer(callback, interval) {
+function Timer(callback, timeInterval) {
     var timerId, totalElapsedTime = 0, startTime, endTime, remaining, remainingMS, remainingDate;
     var state = 0; // 0 = idle, 1 = running, 2 = paused, 3 = resumed
 
@@ -56,10 +55,10 @@ function Timer(callback, interval) {
 
 		var currTime = new Date();
 		remainingMS = currTime - startTime;
+        totalElapsedTime += remainingMS;
+        remaining =  timeInterval - remainingMS;
         console.log("pause() \ncurrtime " + currTime + "\nstartTime " + startTime +
         "\nRemainingMS " + remainingMS + "\nElapsedTime " + totalElapsedTime);
-        totalElapsedTime += remainingMS;
-        //remaining =  interval - remainingMS;
         state = 2;
         window.clearInterval(timerId);
     };
@@ -68,7 +67,7 @@ function Timer(callback, interval) {
         if (state != 2) return;
 
         state = 3;
-        this.timeoutCallback();
+        setTimeout(this.timeoutCallback, remaining);
     };
 
     this.timeoutCallback = function() {
@@ -76,7 +75,7 @@ function Timer(callback, interval) {
 
         //console.log("callback timeoutId BEFORE" + timeoutId);
         //remainingDate = new Date(addMinutes(startTime, 25) - remainingMS);
-        remainingDate = new Date(addMinutes(new Date(), 25) - totalElapsedTime);
+        remainingDate = new Date(addMinutes(new Date(), fullTimerPeriod) - totalElapsedTime);
         timerId = callback(timerDiv, remainingDate);
         startTime = new Date();
         console.log("startTime " + startTime + "\nElapsedTime " + totalElapsedTime);
@@ -84,18 +83,10 @@ function Timer(callback, interval) {
     };
 
     startTime = new Date();
-    endTime = addMinutes(startTime, 25);
+    endTime = addMinutes(startTime, fullTimerPeriod);
     timerId = callback(timerDiv, endTime);
     state = 1;
 }
-
-// to use
-/*
-var timer = new Timer(FUNCTION, 1000);
-timer.pause();
-timer.resume()
- */
-
 
 document.getElementById("timerStart").onclick = startTimer;
 document.getElementById("timerPause").onclick = pauseTimer;
@@ -107,28 +98,18 @@ function startTimer(){
     document.getElementById('timerStarted').style.display = 'block';
     document.getElementById('timerDefault').style.display = 'none';
 
-
-    timer = new Timer(initializeClock, 1000);
+    timer = new Timer(initializeClock, timeInterval);
 
 }
 
-function pauseTimer(){
-	// need to get current date object in order to resume?
-	// clear interval, and then 'initialize clock' with the date
+function pauseTimer() {
     if(timer){
         timer.pause();
     }
 }
 
-function resumeTimer(){
+function resumeTimer() {
     if(timer){
         timer.resume();
     }
 }
-
-//
-//window.setTimeout(function () {
-//    timer.pause(); window.setTimeout( function () {
-//        timer.resume();
-//    }, 5000);
-//}, 2000);
